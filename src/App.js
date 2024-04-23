267731dde5bdd9f59cb6ce4080564bc091a4bb42
Dominicgalan <ndpgalan.student@ua.edu.ph>
2024-04-23 14:43:29 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { useState } from "react";
 
-function App() {
+
+function CalcButton({label, buttonClassName = "CalcButton", onClick}) {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <button className= {buttonClassName} onClick={onClick}>
+      {label}
+    </button>
   );
 }
 
-export default App;
+function CalcDisplay({display}) {
+  return (
+    <div className="CalcDisplay">
+      {display}
+    </div>    
+  );
+}
+
+export default function App() {
+
+  const[disp, setDisp] = useState(0);
+
+  const clickHandler = (e) => {
+    e.preventDefault();
+    const value  = e.target.innerHTML;
+    setDisp(value);
+    //alert('You clicked: ' + value);
+  }
+
+  const numberClickHandler = (e) => {
+    e.preventDefault();
+    const value  = e.target.innerHTML;
+    setDisp(value);
+    //alert('You clicked: ' + value);
+  }
+
+  return (
+    <div className="App">
+      <div className="CalcContainer">
+        <CalcDisplay display={disp} />
+        <div className="ButtonContainer">
+          <CalcButton label={"/"} onClick={clickHandler}/>
+          <CalcButton label={7} onClick={numberClickHandler}/>
+          <CalcButton label={8} onClick={numberClickHandler}/>
+          <CalcButton label={9} onClick={numberClickHandler}/>
+          <CalcButton label={"*"} onClick={clickHandler}/>
+          <CalcButton label={4} onClick={numberClickHandler}/>
+          <CalcButton label={5} onClick={numberClickHandler}/>
+          <CalcButton label={6} onClick={numberClickHandler}/>
+          <CalcButton label={"+"} onClick={clickHandler}/>
+          <CalcButton label={1} onClick={numberClickHandler}/>
+          <CalcButton label={2} onClick={numberClickHandler}/>
+          <CalcButton label={3} onClick={numberClickHandler}/>
+          <CalcButton label={"-"}  onClick={clickHandler}/>
+          <CalcButton label={"C"} onClick={clickHandler}/>
+          <CalcButton label={0} onClick={numberClickHandler}/>
+          <CalcButton label={"="} onClick={clickHandler}/>
+        </div>
+      </div>
+    </div>
+  );
+}
