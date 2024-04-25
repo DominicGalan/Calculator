b32c9c95c71f917c9b59a92d5cc3554fa6b95008
Dominicgalan <ndpgalan.student@ua.edu.ph>
2024-04-25 14:47:03 +0800

    <update>

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import './App.css';
 import { useState } from "react";
 
 
-function CalcButton({color, label, buttonClassName = "CalcButton", onClick}) {
+function CalcButton({lineargradient, label, buttonClassName = "CalcButton", onClick}) {
 
   return (
-    <button className= {buttonClassName} onClick={onClick} style={{ backgroundColor: color }}>
+    <button className= {buttonClassName} onClick={onClick} style={{ background: lineargradient }}>
       {label}
     </button>
   );
@@ -21,18 +21,62 @@ function CalcDisplay({display}) {
 }
 
 export default function App() {
+
   const[disp, setDisp] = useState(0);
 
-  const clickHandler = (e) => {
-    e.preventDefault();
-    const value  = e.target.innerHTML;
-    setDisp(value);
-    //alert('You clicked: ' + value);
+  const[n1, setN1] = useState(null);
+  const[n2, setN2] = useState(null);
+  const[op, setOp] = useState(null);
+
+  const clrClickHandler = (e) => {
+    setDisp(0);
+    setN1(null);
+    setN2(null);
+    setOp(null);
+  
+  }
+
+  const equalClickHandler = (e) => {
+    console.log('Num1 ' + n1 + 'l ' + 'Op ' + op + 'l ' + 'Num2 ' + n2);
+    if (op === "+") {
+      setDisp(parseInt(n1) + parseInt(n2))
+    } else if (op === "-") {
+      setDisp(parseInt(n1) - parseInt(n2))
+    } else if (op === "÷") {
+      setDisp(parseInt(n1) / parseInt(n2))
+    } else if (op === "x") {
+      setDisp(parseInt(n1) * parseInt(n2))
+    } else {
+      setDisp ('Invalid Operation')
+    }
   }
 
   const numberClickHandler = (e) => {
     e.preventDefault();
     const value  = e.target.innerHTML;
+
+    if (op === null) {
+      if (n1 === null){
+        setN1(value);
+        setDisp(value);
+      } else {
+        setN1(n1 + value);
+        setDisp(n1 + value);
+      }
+    } else {
+      if (n2 === null){
+        setN2(value);
+        setDisp(value);
+      } else {
+        setN2(n2 + value);
+        setDisp(n2 + value);
+      }
+    }
+  }
+  const opClickHandler = (e) => {
+    e.preventDefault();
+    const value  = e.target.innerHTML;
+    setOp(value);
     setDisp(value);
     //alert('You clicked: ' + value);
   }
@@ -42,22 +86,22 @@ export default function App() {
       <div className="CalcContainer">
         <CalcDisplay display={disp} />
         <div className="ButtonContainer">
-          <CalcButton label={"/"} onClick={clickHandler} color={'#535dc7'} />
+          <CalcButton label={"÷"} onClick={opClickHandler} lineargradient={'#535dc7'}/>
           <CalcButton label={7} onClick={numberClickHandler}/>
           <CalcButton label={8} onClick={numberClickHandler}/>
           <CalcButton label={9} onClick={numberClickHandler}/>
-          <CalcButton label={"*"} onClick={clickHandler} color={'#535dc7'}/>
+          <CalcButton label={"x"} onClick={opClickHandler} lineargradient={'#535dc7'}/>
           <CalcButton label={4} onClick={numberClickHandler}/>
           <CalcButton label={5} onClick={numberClickHandler}/>
           <CalcButton label={6} onClick={numberClickHandler}/>
-          <CalcButton label={"+"} onClick={clickHandler} color={'#535dc7'}/>
+          <CalcButton label={"+"} onClick={opClickHandler} lineargradient={'#535dc7'}/>
           <CalcButton label={1} onClick={numberClickHandler}/>
           <CalcButton label={2} onClick={numberClickHandler}/>
           <CalcButton label={3} onClick={numberClickHandler}/>
-          <CalcButton label={"-"}  onClick={clickHandler} color={'#535dc7'}/>
-          <CalcButton label={"C"} onClick={clickHandler} color={'#c75353'}/>
+          <CalcButton label={"-"} onClick={opClickHandler} lineargradient={'#535dc7'}/>
+          <CalcButton label={"C"} onClick={clrClickHandler} lineargradient={'#c75353'}/>
           <CalcButton label={0} onClick={numberClickHandler}/>
-          <CalcButton label={"="} onClick={clickHandler} color={'#59c753'}/>
+          <CalcButton label={"="} onClick={equalClickHandler} lineargradient={'#59c753'}/>
         </div>
       </div>
     </div>
