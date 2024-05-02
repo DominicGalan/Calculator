bb7bf5b4c887543c30f0f191eb349caac7aba095
DominicGalan <ndpgalan.student@ua.edu.ph>
2024-05-02 14:38:48 +0800

    <update>

--- src/App.js
+++ src/App.js
@@ -33,27 +33,36 @@ export default function App() {
     setN1(null);
     setN2(null);
     setOp(null);
-  
   }
 
   const equalClickHandler = (e) => {
     console.log('Num1 ' + n1 + 'l ' + 'Op ' + op + 'l ' + 'Num2 ' + n2);
-    if (op === "+") {
-      setDisp(parseInt(n1) + parseInt(n2))
-    } else if (op === "-") {
-      setDisp(parseInt(n1) - parseInt(n2))
-    } else if (op === "÷") {
-      setDisp(parseInt(n1) / parseInt(n2))
-    } else if (op === "x") {
-      setDisp(parseInt(n1) * parseInt(n2))
+
+    let result = null;
+
+    if (op === "ADD") {
+      result = (parseInt(n1) + parseInt(n2))
+    } else if (op === "SUB") {
+      result = (parseInt(n1) - parseInt(n2))
+    } else if (op === "DIV") {
+      result = (parseInt(n1) / parseInt(n2))
+    } else if (op === "MUL") {
+      result = (parseInt(n1) * parseInt(n2))
     } else {
-      setDisp ('Invalid Operation')
+      result = ('Invalid Operation')
     }
+    setDisp(result);
+    setN1(result);
+    setN2(null);
+    setOp(null);
   }
 
   const numberClickHandler = (e) => {
     e.preventDefault();
     const value  = e.target.innerHTML;
+    if (value === "0" && disp === "0") {
+      return;
+    }
 
     if (op === null) {
       if (n1 === null){
@@ -78,30 +87,30 @@ export default function App() {
     const value  = e.target.innerHTML;
     setOp(value);
     setDisp(value);
-    //alert('You clicked: ' + value);
   }
+  
 
   return (
     <div className="App">
       <div className="CalcContainer">
         <CalcDisplay display={disp} />
         <div className="ButtonContainer">
-          <CalcButton label={"÷"} onClick={opClickHandler} lineargradient={'#535dc7'}/>
+          <CalcButton label={"DIV"} onClick={opClickHandler} lineargradient={'#535dc7'}/>
           <CalcButton label={7} onClick={numberClickHandler}/>
           <CalcButton label={8} onClick={numberClickHandler}/>
           <CalcButton label={9} onClick={numberClickHandler}/>
-          <CalcButton label={"x"} onClick={opClickHandler} lineargradient={'#535dc7'}/>
+          <CalcButton label={"MUL"} onClick={opClickHandler} lineargradient={'#535dc7'}/>
           <CalcButton label={4} onClick={numberClickHandler}/>
           <CalcButton label={5} onClick={numberClickHandler}/>
           <CalcButton label={6} onClick={numberClickHandler}/>
-          <CalcButton label={"+"} onClick={opClickHandler} lineargradient={'#535dc7'}/>
+          <CalcButton label={"ADD"} onClick={opClickHandler} lineargradient={'#535dc7'}/>
           <CalcButton label={1} onClick={numberClickHandler}/>
           <CalcButton label={2} onClick={numberClickHandler}/>
           <CalcButton label={3} onClick={numberClickHandler}/>
-          <CalcButton label={"-"} onClick={opClickHandler} lineargradient={'#535dc7'}/>
-          <CalcButton label={"C"} onClick={clrClickHandler} lineargradient={'#c75353'}/>
+          <CalcButton label={"SUB"} onClick={opClickHandler} lineargradient={'#535dc7'}/>
+          <CalcButton label={"CLR"} onClick={clrClickHandler} lineargradient={'#c75353'}/>
           <CalcButton label={0} onClick={numberClickHandler}/>
-          <CalcButton label={"="} onClick={equalClickHandler} lineargradient={'#59c753'}/>
+          <CalcButton label={"EQ"} onClick={equalClickHandler} lineargradient={'#59c753'}/>
         </div>
       </div>
     </div>
